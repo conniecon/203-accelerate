687b45f7eb4e11300d6ccb5aa5e4a31a3d685999
concon <littlemissconnie@yahoo.com>
2018-11-02 19:47:47 -0400

    first update

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -23,13 +23,41 @@ get_header(); ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
+
+<section class="featured-work">
+	<div class="site-content">
+
+		<h3>FEATURED WORK</h3>
+
+		<ul class="homepage-featured-work">
+				<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+					<?php while ( have_posts() ) : the_post(); 
+						$image_1 = get_field("image_1");
+						$size = "medium";
+					?>
+					<li class="individual-featured-work">
+
+						<figure>
+							<?php echo wp_get_attachment_image($image_1, $size); ?>
+						</figure>
+
+						<h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5> 
+					</li>
+					<?php endwhile; //end of the loop.?>
+				<?php wp_reset_query(); //resets the altered query back to the original ?>
+		</ul>
+
+	</div>
+</section>
+
+
 <section class="recent-posts">
 	<div class="site-content">
 		<div class="blog-post">
 			<h4>From the Blog</h4>
 				<?php query_posts('posts_per_page=1'); ?>
 					<?php while ( have_posts() ) : the_post(); ?>
-						<h3><?php the_title(); ?></h3>
+						<h2><?php the_title(); ?></h2>
 						<?php the_excerpt(); ?>
 					<?php endwhile; ?>
 				<?php wp_reset_query(); ?>
